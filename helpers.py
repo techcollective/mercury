464e5a96c55d5cdf21b5dbd631d69289aaf9ccbb
Vasili <vasili@techcollective.com>
2010-05-26 03:45:01 +0000

    refactored some functions to helpers.py

---
+++ helpers.py
@@ -0,0 +1,36 @@
+import decimal
+
+from configuration.models import Config, InvoiceStatus
+
+def get_or_create_default_invoice_status():
+    desired_status = Config.settings.get_setting("default invoice status")
+    status = None
+    if desired_status:
+        status, created = InvoiceStatus.objects.get_or_create(
+            status=desired_status)
+    return status
+
+
+def get_currency_symbol():
+    prefix = ""
+    suffix = ""
+    symbol = Config.settings.get_setting("currency symbol")
+    after_number = False
+    if Config.settings.get_setting("currency symbol after number").lower() == "true":
+        after_number = True
+    if after_number:
+        suffix = symbol
+    else:
+        prefix = symbol
+    return (prefix, suffix)
+
+
+
+def get_tax_percentage():
+    tax_percentage = Config.settings.get_setting("tax as percentage")
+    try:
+        tax_percentage = decimal.Decimal(tax_percentage)
+    except (ValueError, TypeError):
+        tax_percentage = 0
+        # TODO: warn
+    return tax_percentage
