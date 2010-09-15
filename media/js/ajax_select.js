cb9f35acfaf37771493ff2d171da7a3d21d17161
Vasili <vasili@techcollective.com>
2010-09-15 01:20:57 +0000

    slightly improved new ajax, x's and +'s work

--- media/js/ajax_select.js
+++ media/js/ajax_select.js
@@ -1,5 +1,5 @@
 
-/* requires django's RelatedObjectLookups.js */
+/* didAddPopup requires django's RelatedObjectLookups.js */
 
 function didAddPopup(win, newId, newRepr) {
     var name = windowname_to_id(win.name);
@@ -10,6 +10,17 @@ function didAddPopup(win, newId, newRepr) {
 function handleResult(event, ui){
     input_id = event.target.id;
     $("#" + input_id + "_hidden").val(ui.item.pk);
-    $("#" + input_id).val(ui.item.item);
+    $("#" + input_id).val(ui.item.value);
     $("#" + input_id + "_clear").show();
 }
+
+$(document).ready(function(){
+    // Hide X's if the input box is empty
+    $(".ajax-clearfield").each(function(){
+        input_id = this.id;
+        input_id = input_id.replace("_clear", "");
+        if (! $("#" + input_id).val()){
+            $(this).hide();
+        }
+    });
+});
