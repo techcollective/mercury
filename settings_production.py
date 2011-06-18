f18f56007eca1bee79d949969e3e3267cc61c02c
Vasili <vasili@techcollective.com>
2011-06-18 20:19:08 +0300

    adding production settings file

---
+++ settings_production.py
@@ -0,0 +1,19 @@
+# Django settings for mercury.
+
+
+from mercury.settings import *
+
+DEBUG = False
+
+INSTALLED_APPS.remove("django_extensions")
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysql',
+        'NAME': 'mercury',
+        'USER': 'mercury',                      # Not used with sqlite3.
+        'PASSWORD': 'PRODUCTION_PASSWORD_HERE', # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
