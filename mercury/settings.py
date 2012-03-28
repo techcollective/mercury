0f1e4ffbe6e2e3ffa2d3faa4c9362643aae89aee
Vasili <vasili@techcollective.com>
2012-03-28 04:29:39 +0300

    switching to postgresql

--- mercury/settings.py
+++ mercury/settings.py
@@ -40,9 +40,9 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'mercury',
-        'USER': 'mercury',               # Not used with sqlite3.
+        'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
