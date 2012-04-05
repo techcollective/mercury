d826346bc4c125675428e0dab7505821a358116c
Vasili <vasili@techcollective.com>
2012-04-05 15:52:22 +0300

    reinstating dev media urlconf

--- mercury/urls.py
+++ mercury/urls.py
@@ -12,3 +12,13 @@ urlpatterns = patterns('',
     (r'^accounts/', include('accounts.urls')),
     (r'', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    from django.views.static import serve
+    media_url = settings.MEDIA_URL
+    if media_url.startswith('/'):
+        media_url = media_url[1:]
+        urlpatterns += patterns('',
+            (r'^%s(?P<path>.*)$' % media_url,
+             serve,
+             {'document_root': settings.MEDIA_ROOT}))
