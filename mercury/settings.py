5f25b0e47b1de6ba986cc5268f169fafa7f0da8c
Vasili <vasili@techcollective.com>
2012-04-11 21:00:18 +0300

    installing south

--- mercury/settings.py
+++ mercury/settings.py
@@ -153,6 +153,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.admin',
     'django.contrib.staticfiles',
+    'south',
     'ajax_select',
     'configuration',
     'accounts',
