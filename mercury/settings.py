e18b5ce323edbd2633b8823b51c4be906cdef68a
Vasili <vasili@techcollective.com>
2012-04-27 02:35:27 +0300

    adding pandora's box

--- mercury/settings.py
+++ mercury/settings.py
@@ -129,6 +129,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'pandora.middleware.UserMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
