d93031fcea2ca7ca7bc04043bb656b80916e02a2
Vasili <vasili@techcollective.com>
2012-05-02 18:01:36 +0300

    db refresh for admin log message

--- accounts/admin.py
+++ accounts/admin.py
@@ -420,8 +420,10 @@ class ProductOrServiceAdmin(MercuryAdmin):
     filter_horizontal = ["categories"]
 
     def log_change(self, request, object, message):
-        message += " Stock: %s" % object.stock
-        super(ProductOrServiceAdmin, self).log_change(request, object, message)
+        # re-fetch from db to get stock number (might be an F() expression)
+        obj = self.model.objects.get(pk=object.pk)
+        message += " Stock: %s" % obj.stock
+        super(ProductOrServiceAdmin, self).log_change(request, obj, message)
 
 
 class SalesReportAdmin(MercuryAjaxAdmin):
