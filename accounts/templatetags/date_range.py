8c9958a217de408671338f87cfceba4e28c4f3e3
Vasili <vasili@techcollective.com>
2012-05-11 00:13:49 +0300

    adding last month, last year shortcuts to date_range

--- accounts/templatetags/date_range.py
+++ accounts/templatetags/date_range.py
@@ -26,8 +26,20 @@ def context_helper(start_field, end_field):
     This is an attempt to make context variables needed by the date_range.html
     template available to views that aren't going to use the template tag.
     """
-    # figure out start and end dates of last quarter for the shortcut link
     today = datetime.date.today()
+
+    # figure out start and end dates of last month
+    last_month = today.month - 1
+    if last_month == 0:
+        lm_year = today.year - 1
+        last_month = 12
+    else:
+        lm_year = today.year
+    lm_end_day = calendar.monthrange(lm_year, last_month)[1]
+    last_month_start = datetime.date(lm_year, last_month, 1)
+    last_month_end = datetime.date(lm_year, last_month, lm_end_day)
+
+    # figure out start and end dates of last quarter for the shortcut link
     # lq = last quarter number (1-4)
     lq = ((today.month - 1) // 3)
     if lq == 0:
@@ -37,14 +49,24 @@ def context_helper(start_field, end_field):
         lq_year = today.year
     lq_end_month = lq * 3
     lq_start_month = lq_end_month - 2
-    # find out many days are in lq_end_month
+    # find out how many days are in lq_end_month
     lq_end_day = calendar.monthrange(lq_year, lq_end_month)[1]
     last_quarter_start = datetime.date(lq_year, lq_start_month, 1)
     last_quarter_end = datetime.date(lq_year, lq_end_month, lq_end_day)
+
+    # start and end for last year
+    last_year = today.year - 1
+    last_year_start = datetime.date(last_year, 1, 1)
+    last_year_end = datetime.date(last_year, 12, 31)
+
     return {"start_field": "id_" + start_field,
             "end_field": "id_" + end_field,
+            "last_month_start": last_month_start,
+            "last_month_end": last_month_end,
             "last_quarter_start": last_quarter_start,
             "last_quarter_end": last_quarter_end,
+            "last_year_start": last_year_start,
+            "last_year_end": last_year_end,
             }
 
 
